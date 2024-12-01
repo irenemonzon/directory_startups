291b5fb7a56b78f9186b0c8db2d2409ddd8d85a2
irenemonzon <irene.monzonm@gmail.com>
2024-12-01 19:46:10 +1100

    create and add search form and install shadcn/ui library

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import SearchForm from "../../components/SearchForm";
 
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query?:string}>
+}) {
+
+  const query=(await searchParams).query
+
+
+
   return (
-  <h1 className="text-3xl">Welcome to Next.js</h1>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your Startup, <br/>Connect with Entrepeneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in virtual competitions</p>
+        <SearchForm query={query}/>
+      </section>
+    </>
   );
 }
